630f7dcd78ad103cac82b74909a1d2bbc9e3004c
Marcin Kadłubowski <cinek260@gmail.com>
2016-01-17 02:45:14 +0100

    fix

--- src/modal.react.js
+++ src/modal.react.js
@@ -19,7 +19,7 @@ var _react = require('react');
 
 var _react2 = _interopRequireDefault(_react);
 
-require('./modal.styl');
+require('../assets/modal.styl');
 
 Modal = (function (_Component) {
   _inherits(_temporalAssertDefined(Modal, 'Modal', _temporalUndefined) && Modal, _Component);
